d5e80d1c2941051e5411727ef0ddd9724b64cf2c
Helton Nogueira <helton.soares.nog@gmail.com>
2026-02-07 23:29:08 -0400

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,21 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+
+/// AAA - Arrange, Act, Assert
+
 test('Consultar Pedidos', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
   
+// Arrange
+ await page.goto('http://localhost:5173/');
   // Checkpoint: Verifica se o título da página é "Velô Sprint"
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  
-  // Checkpoint: Verifica se a página de consulta de pedidos é carregada
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
  
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-7LDWFT');
-
-
   await page.getByTestId('search-order-button').click();
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-7LDWFT');
 
